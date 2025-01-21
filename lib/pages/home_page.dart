25795c17f37f16dc54478bbb38b9a1c9bce332e1
MostafaHasan1 <hasanww2@gmail.com>
2025-01-22 05:16:45 +0600

    how to create task and checkbox enabled

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  //list of to do task
+  List toDoList = [
+    ["make tutorial", false],
+    ["do exercise", false]
+  ];
+
+  //checkbox was tapped
+
+  void checkBoxChanged(bool? value, int index){
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +32,11 @@ class _HomePageState extends State<HomePage> {
         title: const Center(child: Text("ToDo", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),)),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          TodoTile(),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return TodoTile(taskName: toDoList[index][0], taskCompleted: toDoList[index][1], onChanged: (value) => checkBoxChanged(value, index));
+        },
       ),
     );
   }
--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -16,16 +16,17 @@ class TodoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
       child: Container(
         padding: EdgeInsets.all(25),
         child: Row(
           children: [
             //checkboox
-            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Checkbox(value: taskCompleted, onChanged: onChanged, activeColor: Colors.black,),
 
             //taks name
-            Text(taskName),
+            Text(taskName,
+            style: TextStyle(decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none),),
           ],
         ),
         decoration: BoxDecoration(
